3fd3e8f0bf7b3c0f1a4c47d0a265130187e80b92
zhengchao <chao.zheng@okcoin.com>
2020-03-24 22:53:01 +0800

    修改路由

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-from crate.theme.rtd.conf.crate_reference import *
+from okchain1.theme.rtd.conf.crate_reference import *
 
 source_suffix = '.rst'
 site_url = 'https://crate.io/docs/crate/reference/en/latest/'
