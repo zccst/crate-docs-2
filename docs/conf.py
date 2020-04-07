f1c6674e471b967fdc93fa18028f0cb5216a2897
zhengchao <chao.zheng@okcoin.com>
2020-04-07 20:03:39 +0800

    配置修改，add

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,34 @@
 from okchain1.theme.rtd.conf.okchain_docs import *
 
+import sphinx_rtd_theme
+
 source_suffix = '.rst'
 site_url = 'https://zc-crate-docs-04.readthedocs.io/en/stable/'
 
 exclude_patterns = ['out/**', 'tmp/**', 'eggs/**', 'requirements.txt', 'README.rst']
 
-extensions = ['crate.sphinx.csv', 'sphinx_sitemap']
+extensions = [
+    'crate.sphinx.csv',
+    'sphinx_sitemap'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx_markdown_tables',
+]
 
 # crate.theme sets html_favicon to favicon.png which causes a warning because
 # it should be a .ico and in addition there is no favicon.png in this project
 # so it can't find the file
 html_favicon = None
+
+
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = "sphinx_rtd_theme"
