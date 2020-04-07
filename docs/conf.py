a3d017beb558f50af05e2ab526343eb56cf6dbf2
zhengchao <chao.zheng@okcoin.com>
2020-04-07 20:14:19 +0800

    配置修改，remove主题

--- docs/conf.py
+++ docs/conf.py
@@ -31,4 +31,4 @@ html_favicon = None
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
